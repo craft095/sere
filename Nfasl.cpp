916037ca346561b0ede04962c9051799eacc5f37
Dmitry Kulagin <dmitry.kulagin@gmail.com>
2020-03-04 23:03:44 +0300

    Introduce Catch2 test environment

--- Nfasl.cpp
+++ Nfasl.cpp
@@ -60,4 +60,28 @@ namespace nfasl {
 
 } // namespace nfasl
 
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+#define CATCH_CONFIG_RUNNER
+#include "catch2/catch.hpp"
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
+
+
+int main( int argc, char* argv[] ) {
+  // global setup...
+
+  int result = Catch::Session().run( argc, argv );
+
+  // global clean-up...
+
+  return result;
+}
 #endif // NFASL_HPP
